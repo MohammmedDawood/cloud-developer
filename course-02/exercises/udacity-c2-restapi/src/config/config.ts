b61429321989d1b27480c0a1d5fe1dd279f730ad
Mohammed Dawood <mahmedtony73@gmail.com>
2023-01-08 07:31:47 +0200

    add env

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   dev: {
-    username: "udagramdawooddev",
-    password: "udagramdawooddev",
-    database: "udagramdawooddev",
-    host: "udagramdawooddev.civ2bp0vdefo.us-east-1.rds.amazonaws.com",
-    dialect: "postgres",
-    aws_region: "us-east-1",
-    aws_profile: "default",
-    aws_media_bucket: "udagram-dawood-dev",
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.POSTGRES_dialect,
+    aws_region: process.env.AWS_REGION,
+    aws_profile: process.env.AWS_PROFILE,
+    aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
   },
   jwt: {
     secret: " ",
