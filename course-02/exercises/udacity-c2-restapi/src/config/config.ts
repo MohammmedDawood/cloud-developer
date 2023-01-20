fcaaee7cb22a272fa1995e0d66fd2054fba5ddcb
Mohammed Dawood <mahmedtony73@gmail.com>
2023-01-20 05:42:09 +0200

    add secret key for token

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -9,14 +9,14 @@ export const config = {
     aws_profile: process.env.AWS_PROFILE,
     aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
   },
-  jwt: {
-    secret: " ",
-  },
   prod: {
     username: "",
     password: "",
     database: "udagram_prod",
     host: "",
     dialect: "postgres",
+  },
+  jwt: {
+    secret: "helloworld",
   },
 };
